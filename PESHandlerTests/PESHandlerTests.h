8728cceada85b1c2ea8c1ae9251009587db899da
Raphael Schweikert <any@sabberworm.com>
2011-04-17 18:32:29 +0200

    Initial Commit

---
+++ PESHandlerTests/PESHandlerTests.h
@@ -0,0 +1,17 @@
+//
+//  PESHandlerTests.h
+//  PESHandlerTests
+//
+//  Created by Raphael Schweikert on 17.04.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface PESHandlerTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ PESHandlerTests/PESHandlerTests.m
@@ -0,0 +1,33 @@
+//
+//  PESHandlerTests.m
+//  PESHandlerTests
+//
+//  Created by Raphael Schweikert on 17.04.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "PESHandlerTests.h"
+
+
+@implementation PESHandlerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PESHandlerTests");
+}
+
+@end
