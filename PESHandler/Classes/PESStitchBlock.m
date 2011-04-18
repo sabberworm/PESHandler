ee0591ae40461636efc95f0628814d6b270a61b4
Raphael Schweikert <any@sabberworm.com>
2011-04-18 07:17:10 +0200

    implemented pathFromBlock…

--- PESHandler/Classes/PESStitchBlock.m
+++ PESHandler/Classes/PESStitchBlock.m
@@ -8,6 +8,8 @@
 
 #import "PESStitchBlock.h"
 
+#import "PESStitch.h"
+
 @implementation PESStitchBlock
 
 @synthesize color;
@@ -31,6 +33,28 @@
 
 - (void) addStitch:(PESStitch *) stitch {
 	[stitches addObject:stitch];
+}
+
+- (NSBezierPath *) pathFromBlockWithScale:(CGFloat)scale {
+	return [self pathFromBlockXOffset:0 yOffset:0 andScale:scale];
+}
+
+- (NSBezierPath *) pathFromBlockXOffset:(NSUInteger)xOffset yOffset:(NSInteger) yOffset andScale:(CGFloat)scale {
+	NSBezierPath *result = [[NSBezierPath alloc] init];
+	
+	BOOL first = YES;
+	for(PESStitch *stitch in stitches) {
+		CGFloat x = stitch.x + xOffset;
+		CGFloat y = stitch.y + yOffset;
+		if(first) {
+			[result moveToPoint:(NSPoint){x, y}];
+			first = NO;
+		} else {
+			[result lineToPoint:(NSPoint){x, y}];
+		}
+	}
+	
+	return result;
 }
 
 @end
